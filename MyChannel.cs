8029b5d3cda487554c87e93e5cf1fa6815a300d2
Rafael Pimenta <rafael.pimenta@hexagon.com>
2023-11-07 18:14:14 -0300

    implementation of simply channel with consumer and publisher

---
+++ MyChannel.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Concurrent;
+
+namespace Channel;
+
+
+public class ChannelProducer
+{
+    public static async Task Producer(IWrite<string?> writer)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            writer.Push(i.ToString());
+            await Task.Delay(100);
+        }
+        writer.Complete();
+    }
+}
+
+public class ChannelConsumer
+{
+    public static async Task Consumer(IRead<string?> reader)
+    {
+        while (!reader.IsCompleted)
+        {
+            var value = await reader.ReadAsync();
+            Console.WriteLine($"message: {value}");
+        }
+    }
+}
+
+public interface IRead<T>
+{
+    Task<T?> ReadAsync();
+    bool IsCompleted { get; }
+}
+
+public interface IWrite<T>
+{
+    void Push(T? value);
+    void Complete();
+}
+
+public class Channel<T> : IRead<T?>, IWrite<T>
+{
+    private bool _finished;
+    private readonly ConcurrentQueue<T?> _queue = new();
+    private readonly SemaphoreSlim _flag = new(0);
+
+    public void Complete()
+    {
+        _finished = true;
+    }
+
+    public bool IsCompleted => _finished && _queue.IsEmpty;
+
+    public void Push(T? value)
+    {
+        _queue.Enqueue(value);
+        _flag.Release();
+    }
+
+    public async Task<T?> ReadAsync()
+    {
+        await _flag.WaitAsync();
+        if (_queue.TryDequeue(out T? value))
+        {
+            return value;
+        }
+        return value;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Channels;
+
+namespace Channel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = new Channel<string?>();
+
+            Task.WaitAll(ChannelConsumer.Consumer(channel), ChannelProducer.Producer(channel), ChannelProducer.Producer(channel), ChannelProducer.Producer(channel));
+            
+        }
+    }
+}
